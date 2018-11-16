bd11ea8572e04131ca498c35cfa99d1c71ba8848
matelliohimmat <himmat.singh@matellio.com>
2018-11-16 12:19:24 +0530

    Solidity contract updated

--- blockchain/contracts/Trade.sol
+++ blockchain/contracts/Trade.sol
@@ -20,7 +20,17 @@ contract AssetContract{
     function GetAsset(uint16 _sku) constant public returns(uint16 _asset,uint256 _price, address _owner, uint32 _lastModifiedOn){
         Asset storage asset = assets[_sku];
         return (asset.sku,asset.price, asset.owner, asset.lastModifiedOn);
-    }    
+    }
+
+    function RegisterAsset(uint16 _sku,uint256 _price, address _owner, uint32 _lastModifiedOn) public{
+        assets[_sku] = Asset({sku:_sku,price:_price, owner:_owner, lastModifiedOn:_lastModifiedOn});
+        CreateAsset(_sku,_price, _owner, _lastModifiedOn);
+    }
+
+    function TransferAssetOwner(address _newOwner, address _previousOwner, uint16 _sku) public{
+        assets[_sku].owner = _newOwner;
+        TransferAssetOwnership(_newOwner,_previousOwner,_sku);
+    }  
 }
 
 contract UserContract{
@@ -60,43 +70,29 @@ contract TradeContract is AssetContract, UserContract{
         address buyer;
         address seller;
         uint16 sku;
-        uint8 orderDate;
+        uint32 orderDate;
     }
 
     mapping(uint16 => Order) orders;
-    enum OrderStatus {Created, Paid, Completed}
 
     function TradeContract() public{
 
     }
 
-    modifier IsValidUser(address _userAddress){
-        if(msg.sender != _userAddress){
-            throw;
-        }
-        _;
-    }
-
-    
-
-    event CreateOrder(uint16 _orderId,uint _orderAmount, uint _orderStatus, address buyer, address seller, uint16 _sku, uint _orderDate);
+    event CreateOrder(uint16 _orderId,uint _orderAmount, uint _orderStatus, address buyer, address seller, uint16 _sku, uint32 _orderDate);
     
     function GetOrder(uint16 _orderId) constant public returns(uint256 _orderAmount, uint8 _orderStatus, address buyer, address seller, uint16 _sku, uint32 _orderDate){
         Order storage order= orders[_orderId];
         return (order.orderAmount, order.orderStatus, order.buyer, order.seller, order.sku, order.orderDate);
     }
 
-    function GenerateOrder(uint16 _orderId,uint256 _orderAmount, uint8 _orderStatus, address _buyer, address _seller, uint16 _sku, uint8 _orderDate ) IsValidUser(_buyer) payable public{
+    function GenerateOrder(uint16 _orderId,uint256 _orderAmount, uint8 _orderStatus, address _buyer, address _seller, uint16 _sku, uint32 _orderDate ) payable public{
         orders[_orderId] = Order({orderId:_orderId,orderAmount:_orderAmount, orderStatus:_orderStatus, buyer:_buyer, seller:_seller, sku:_sku, orderDate:_orderDate});
         users[_buyer].userBalance-=_orderAmount;
         users[_seller].userBalance+=_orderAmount;
         
+        TransferAssetOwner(_buyer,_seller,_sku);
         CreateOrder(_orderId,_orderAmount, _orderStatus, _buyer, _seller, _sku, _orderDate);
-        TransferAssetOwnership(_buyer,_seller,_sku);
-    }
-
-    function RegisterAsset(uint16 _sku,uint256 _price, address _owner, uint32 _lastModifiedOn) IsValidUser(_owner) public{
-        assets[_sku] = Asset({sku:_sku,price:_price, owner:_owner, lastModifiedOn:_lastModifiedOn});
-        CreateAsset(_sku,_price, _owner, _lastModifiedOn);
-    }    
+        
+    }        
 }
