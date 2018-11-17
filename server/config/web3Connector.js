5a12fe2c5aa61f0c6cd441716aaf6da15f80c70c
matelliohimmat <himmat.singh@matellio.com>
2018-11-17 06:38:12 +0530

    web3 connector updated

--- server/config/web3Connector.js
+++ server/config/web3Connector.js
@@ -1,7 +1,7 @@
 var exports = (module.exports = {});
 var Web3 = require("web3");
 var web3Address = "http://localhost:7545";
-var contractAddress = "0xc56742aba094375998bc3d428a7af76758713fca";
+var contractAddress = "0x1e6be09283cc020872ea639f5bfce28d741e3e95";
 var web3 = new Web3(new Web3.providers.HttpProvider(web3Address));
 var defaultAccount = web3.eth.accounts[0];
 
@@ -9,165 +9,9 @@ var defaultAccount = web3.eth.accounts[0];
 web3.eth.defaultAccount = web3.eth.accounts[0];
 
 // init the trade contract
-var tradecontractContract = web3.eth.contract([
-  {
-    constant: false,
-    inputs: [
-      { name: "_sku", type: "uint16" },
-      { name: "_price", type: "uint256" },
-      { name: "_owner", type: "address" },
-      { name: "_lastModifiedOn", type: "uint32" }
-    ],
-    name: "RegisterAsset",
-    outputs: [],
-    payable: false,
-    type: "function",
-    stateMutability: "nonpayable"
-  },
-  {
-    constant: false,
-    inputs: [
-      { name: "_orderId", type: "uint16" },
-      { name: "_orderAmount", type: "uint256" },
-      { name: "_orderStatus", type: "uint8" },
-      { name: "_buyer", type: "address" },
-      { name: "_seller", type: "address" },
-      { name: "_sku", type: "uint16" },
-      { name: "_orderDate", type: "uint32" }
-    ],
-    name: "GenerateOrder",
-    outputs: [],
-    payable: true,
-    type: "function",
-    stateMutability: "payable"
-  },
-  {
-    constant: true,
-    inputs: [{ name: "_userAddress", type: "address" }],
-    name: "GetUser",
-    outputs: [
-      { name: "_userAddr", type: "address" },
-      { name: "_userBalance", type: "uint256" }
-    ],
-    payable: false,
-    type: "function",
-    stateMutability: "view"
-  },
-  {
-    constant: false,
-    inputs: [
-      { name: "_userAddress", type: "address" },
-      { name: "_initialBalance", type: "uint256" }
-    ],
-    name: "RegisterUser",
-    outputs: [],
-    payable: false,
-    type: "function",
-    stateMutability: "nonpayable"
-  },
-  {
-    constant: false,
-    inputs: [
-      { name: "_newOwner", type: "address" },
-      { name: "_previousOwner", type: "address" },
-      { name: "_sku", type: "uint16" }
-    ],
-    name: "TransferAssetOwner",
-    outputs: [],
-    payable: false,
-    type: "function",
-    stateMutability: "nonpayable"
-  },
-  {
-    constant: true,
-    inputs: [{ name: "_orderId", type: "uint16" }],
-    name: "GetOrder",
-    outputs: [
-      { name: "_orderAmount", type: "uint256" },
-      { name: "_orderStatus", type: "uint8" },
-      { name: "buyer", type: "address" },
-      { name: "seller", type: "address" },
-      { name: "_sku", type: "uint16" },
-      { name: "_orderDate", type: "uint32" }
-    ],
-    payable: false,
-    type: "function",
-    stateMutability: "view"
-  },
-  {
-    constant: true,
-    inputs: [{ name: "_sku", type: "uint16" }],
-    name: "GetAsset",
-    outputs: [
-      { name: "_asset", type: "uint16" },
-      { name: "_price", type: "uint256" },
-      { name: "_owner", type: "address" },
-      { name: "_lastModifiedOn", type: "uint32" }
-    ],
-    payable: false,
-    type: "function",
-    stateMutability: "view"
-  },
-  {
-    constant: true,
-    inputs: [{ name: "_userAddress", type: "address" }],
-    name: "GetUserBalance",
-    outputs: [{ name: "", type: "uint256" }],
-    payable: false,
-    type: "function",
-    stateMutability: "view"
-  },
-  {
-    inputs: [],
-    type: "constructor",
-    payable: true,
-    stateMutability: "payable"
-  },
-  {
-    anonymous: false,
-    inputs: [
-      { indexed: false, name: "_orderId", type: "uint16" },
-      { indexed: false, name: "_orderAmount", type: "uint256" },
-      { indexed: false, name: "_orderStatus", type: "uint256" },
-      { indexed: false, name: "buyer", type: "address" },
-      { indexed: false, name: "seller", type: "address" },
-      { indexed: false, name: "_sku", type: "uint16" },
-      { indexed: false, name: "_orderDate", type: "uint32" }
-    ],
-    name: "CreateOrder",
-    type: "event"
-  },
-  {
-    anonymous: false,
-    inputs: [
-      { indexed: false, name: "_userAddress", type: "address" },
-      { indexed: false, name: "_userBalance", type: "uint256" }
-    ],
-    name: "CreateUser",
-    type: "event"
-  },
-  {
-    anonymous: false,
-    inputs: [
-      { indexed: false, name: "_newOwner", type: "address" },
-      { indexed: false, name: "_previousOwner", type: "address" },
-      { indexed: false, name: "_sku", type: "uint16" }
-    ],
-    name: "TransferAssetOwnership",
-    type: "event"
-  },
-  {
-    anonymous: false,
-    inputs: [
-      { indexed: false, name: "_sku", type: "uint16" },
-      { indexed: false, name: "_price", type: "uint256" },
-      { indexed: false, name: "_owner", type: "address" },
-      { indexed: false, name: "_lastModifiedOn", type: "uint32" }
-    ],
-    name: "CreateAsset",
-    type: "event"
-  }
-]);
+var tradecontractContract = web3.eth.contract([{"constant":false,"inputs":[{"name":"_sku","type":"uint16"},{"name":"_price","type":"uint256"},{"name":"_owner","type":"address"},{"name":"_lastModifiedOn","type":"uint32"}],"name":"RegisterAsset","outputs":[],"payable":false,"type":"function","stateMutability":"nonpayable"},{"constant":false,"inputs":[{"name":"_orderId","type":"uint16"},{"name":"_orderAmount","type":"uint256"},{"name":"_buyer","type":"address"},{"name":"_seller","type":"address"},{"name":"_sku","type":"uint16"}],"name":"GenerateOrder","outputs":[],"payable":true,"type":"function","stateMutability":"payable"},{"constant":true,"inputs":[{"name":"_userAddress","type":"address"}],"name":"GetUser","outputs":[{"name":"_userAddr","type":"address"},{"name":"_userBalance","type":"uint256"}],"payable":false,"type":"function","stateMutability":"view"},{"constant":false,"inputs":[{"name":"_userAddress","type":"address"},{"name":"_initialBalance","type":"uint256"}],"name":"RegisterUser","outputs":[],"payable":false,"type":"function","stateMutability":"nonpayable"},{"constant":false,"inputs":[{"name":"_newOwner","type":"address"},{"name":"_previousOwner","type":"address"},{"name":"_sku","type":"uint16"}],"name":"TransferAssetOwner","outputs":[],"payable":false,"type":"function","stateMutability":"nonpayable"},{"constant":true,"inputs":[{"name":"_orderId","type":"uint16"}],"name":"GetOrder","outputs":[{"name":"_orderAmount","type":"uint256"},{"name":"buyer","type":"address"},{"name":"seller","type":"address"},{"name":"_sku","type":"uint16"}],"payable":false,"type":"function","stateMutability":"view"},{"constant":true,"inputs":[{"name":"_sku","type":"uint16"}],"name":"GetAsset","outputs":[{"name":"_asset","type":"uint16"},{"name":"_price","type":"uint256"},{"name":"_owner","type":"address"},{"name":"_lastModifiedOn","type":"uint32"}],"payable":false,"type":"function","stateMutability":"view"},{"constant":true,"inputs":[{"name":"_userAddress","type":"address"}],"name":"GetUserBalance","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function","stateMutability":"view"},{"inputs":[],"type":"constructor","payable":true,"stateMutability":"payable"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_orderId","type":"uint16"},{"indexed":false,"name":"_orderAmount","type":"uint256"},{"indexed":false,"name":"buyer","type":"address"},{"indexed":false,"name":"seller","type":"address"},{"indexed":false,"name":"_sku","type":"uint16"}],"name":"CreateOrder","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_userAddress","type":"address"},{"indexed":false,"name":"_userBalance","type":"uint256"}],"name":"CreateUser","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_newOwner","type":"address"},{"indexed":false,"name":"_previousOwner","type":"address"},{"indexed":false,"name":"_sku","type":"uint16"}],"name":"TransferAssetOwnership","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"_sku","type":"uint16"},{"indexed":false,"name":"_price","type":"uint256"},{"indexed":false,"name":"_owner","type":"address"},{"indexed":false,"name":"_lastModifiedOn","type":"uint32"}],"name":"CreateAsset","type":"event"}]);
+
+
 var tradeContract = tradecontractContract.at(contractAddress);
 
 /*---------------Functions---------------------*/
@@ -186,20 +30,20 @@ exports.CreateUser = function(_userAddress, balance) {
 exports.CreateOrder = function(
   _orderId,
   _orderAmount,
-  _orderStatus,
+  //_orderStatus,
   _buyer,
   _seller,
-  _sku,
-  _orderDate
+  _sku
+  //, _orderDate
 ) {
   tradeContract.GenerateOrder(
     _orderId,
     _orderAmount,
-    _orderStatus,
+    //_orderStatus,
     _buyer,
     _seller,
-    _sku,
-    _orderDate
+    _sku
+    //,_orderDate
   );
 };
 
